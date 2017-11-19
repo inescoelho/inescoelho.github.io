5aa233e78c264c79cdb444211e071d9ddc9c889c
Ines <ines.opcoelho@gmail.com>
2017-11-19 00:42:07 +0000

    refactor portfolio as a carousel

--- js/profile.js
+++ js/profile.js
@@ -16,3 +16,30 @@ if($('.navbar').length > 0){
         checkScroll();
     });
 }
+
+$('.responsive').slick({
+  dots: true,
+  infinite: true,
+  speed: 300,
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2,
+        slidesToScroll: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+    // You can unslick at a given breakpoint now by adding:
+    // settings: "unslick"
+    // instead of a settings object
+  ]
+});
